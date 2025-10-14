aec0a33fb7dd3da40f192a27044e721fa539e151
Ayee813 <yongyeelove813@gmail.com>
2025-10-14 12:51:07 +0700

    update loading

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Loading.css";
+
+export default function Loading() {
+  return (
+    <div className="absolute root-container">
+      <div className=" z-10 main-container mb-4"></div>
+      <p>Loading...</p>
+    </div>
+  );
+}
--- src/pages/AboutUsPage.tsx
+++ src/pages/AboutUsPage.tsx
@@ -1,9 +1,33 @@
+import { useState, useEffect } from "react";
+import "./AboutUsPage.css";
+import Loading from "@/components/Loading/Loading";
+import { Divide, TurkishLira } from "lucide-react"; // optional
 
 export default function AboutUsPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const delay = 3000; // 3 seconds
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, delay);
+
+    // Cleanup when component unmounts
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="text-4xl font-bold">CEIT CYBER SECURITY</h1>
-      <p className="mt-4 text-lg">We are dedicated to providing the best service possible.</p>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 relative">
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div>
+          <h1 className="text-4xl font-bold">CEIT CYBER SECURITY</h1>
+          <p className="mt-4 text-lg">
+            We are dedicated to providing the best service possible.
+          </p>
+        </div>
+      )}
     </div>
-  )
+  );
 }
